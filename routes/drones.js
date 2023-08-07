3d596db9911539c8151db3fe463b0e10c6ea78c2
kekonline <kekonline@gmail.com>
2023-08-07 17:21:05 +0200

    Done till 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("../models/Drone.model.js");
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+
+Drone.find()
+.then((responseFind)=>{
+res.render("drones/list.hbs",{ allDrones: responseFind});
+
+})
+.catch((error)=>next(error));
 });
 
 router.get('/drones/create', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
 // Iteration #1
+
+const allDrones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model.js");
+
+
+const startSeeding = async () => {
+    try {
+        const responseConection = await mongoose.connect("mongodb://127.0.0.1:27017/lab-express-drones");
+        console.log("Connected to MongoDB" + responseConection);
+        const responseInsert = await Drone.insertMany(allDrones);
+        console.log(responseInsert);
+         const responseCloseConection = await mongoose.connection.close();
+        // console.log(responseCloseConection);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startSeeding();
